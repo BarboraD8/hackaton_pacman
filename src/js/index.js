3f733e59cdb405d7b71ffa69086c74461fb29067
Barbora Chreno Dankova <barboradankova@MyMacBook.local>
2021-10-01 12:57:12 +0200

    commit

--- src/js/index.js
+++ src/js/index.js
@@ -37,28 +37,52 @@ class Pacman {
     mount(){
         this.render();
         this.moveRight();
+        this.moveLeft();
+        this.update();
         return this.element;
     }
 
     update(){
-   
+      this.pac.style.left = `${this.xposition}px`;
     }
 
     moveRight() {
         document.addEventListener('keydown', (event) => {
         if (event.code === 'ArrowRight') {
         this.xposition += 85;
-        this.pac.style.left = `${this.xposition}px`;
-        mouseopen = !mouseopen;
-
-        if (mouth === true) {
+       // this.pac.style.left = `${this.xposition}px`;
+        this.mouth = !this.mouth;
+           
+        if (this.mouth === true) {
             pacBoy.style.backgroundPositionX = 'right';
         } else {
             pacBoy.style.backgroundPositionX = 'left';
-        }})
+        }
+    }
         
+    })
     }
+
+
+    moveLeft() {
+        document.addEventListener('keydown', (event) => {
+        if (event.code === 'ArrowLeft') {
+        this.xposition -= 85;
+       // this.pac.style.left = `${this.xposition}px`;
+        this.mouth = !this.mouth;
+           
+        if (this.mouth === true) {
+            pacBoy.style.backgroundPositionX = 'right';
+        } else {
+            pacBoy.style.backgroundPositionX = 'left';
+        }
+        this.update()
     }
+        
+    })
+    }
+
+
 }
 
 
