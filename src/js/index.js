4983f3cf031b29fa68353797d63b328489bfeb16
Barbora Chreno Dankova <barboradankova@MyMacBook.local>
2021-10-01 10:44:43 +0200

    pacman closing mouth

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,28 @@
+
 'use strict';
 
-console.log('it works!');
+const pacBoy = document.querySelector('.pacboy-active-light');
+const pac = document.querySelector('.entity--pac');
+let mouseopen = false;
+let xpos = 0;
+
+
+
+document.addEventListener('keydown', (event) => {
+    if (event.code === 'ArrowRight') {
+        xpos += 85;
+        pac.style.left = `${xpos}px`;
+        if (mouseopen === true) {
+            pacBoy.style.backgroundPositionX = 'right';
+        } else {
+            pacBoy.style.backgroundPositionX = 'left';
+        }
+        mouseopen = !mouseopen
+    }
+});
+
+
+class Pacman {
+    constructor() {}
+}
+
